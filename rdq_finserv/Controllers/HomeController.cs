07aa3c08cbf34384c00cdb29713fbc990575d8f4
rdq_sanhita <rdq_sanhita@DESKTOP-I46EE5L>
2021-11-08 17:04:29 +0530

    Add project files.

---
+++ rdq_finserv/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rdq_finserv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            ViewBag.Message = "About Us";
+
+            return View();
+        }
+
+        public ActionResult personalloan()
+        {
+            ViewBag.Message = "Personal Loan";
+            return View();
+        }
+        public ActionResult emiprotection()
+        {
+            ViewBag.Message = "Emi Protection";
+            return View();
+        }
+        public ActionResult products()
+        {
+            ViewBag.Message = "Products";
+            return View();
+        }
+        public ActionResult creditcard()
+        {
+            ViewBag.Message = "Credit Card";
+            return View();
+        }
+
+        public ActionResult las()
+        {
+            ViewBag.Message = "Loan Against Securities";
+            return View();
+        }
+
+        public ActionResult consumerdurable()
+        {
+            ViewBag.Message = "Consumer Durable";
+            return View();
+        }
+        public ActionResult Support()
+        {
+            ViewBag.Message = "Support";
+            return View();
+        }
+      
+
+        public ActionResult SignUp()
+        {
+            
+            return View();
+        }
+    }
+}
